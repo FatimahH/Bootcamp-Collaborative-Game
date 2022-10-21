ddf6fd94904e389b8abcc72ae6f2cb847ee83d2d
FatimahH <fadiym@hotmail.com>
2022-10-21 19:37:19 +0300

    project done

--- Assets/Scripts/Network/Spawner.cs
+++ Assets/Scripts/Network/Spawner.cs
@@ -9,6 +9,8 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
 {
     [SerializeField] NetworkPlayer playerPrefab;
 
+    CharacterInputHandler characterInputHandler;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,8 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
         Debug.Log("OnConnectedToServer");
     }
 
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
         if (runner.IsServer)
         {
             Debug.Log("OnPlayerJoined we are server. Spawning Player");
@@ -28,7 +31,18 @@ public class Spawner : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
-    public void OnInput(NetworkRunner runner, NetworkInput input) { }
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        if(characterInputHandler == null && NetworkPlayer.Local != null)
+        {
+            characterInputHandler = NetworkPlayer.Local.GetComponent<CharacterInputHandler>();
+        } 
+
+        if(characterInputHandler != null)
+        {
+            input.Set(characterInputHandler.GetNetworkInput());
+        }
+    }
     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
     {
